ef8bf2c9be829cf68b0c755275d220ddc06a97e8
Kohji OKAMURA <okamura-k@ncchd.go.jp>
2021-12-11 01:19:02 +0900

    Prepare data for gender CNN

--- anaysis/gender/CNN/prepare_training_and_test_data.py
+++ anaysis/gender/CNN/prepare_training_and_test_data.py
@@ -5,97 +5,83 @@ prepare_training_and_test_data.py
 prepare data for leave-one-out cross-validation of the 2429 images
 '''
 
+__author__  = 'Kohji'
 __version__ = 0.1
 __date__    = '2020-04-25'
-__author__  = 'Kohji'
-
 __version__ = 0.2
 __date__    = '2020-04-27'
-
 __version__ = 0.3
 __date__    = '2020-04-28'
+__version__ = 0.4
+__date__    = '2021-12-10'
 
-file_images  = './head_shot_data.npy'
-file_labels  = './head_shot_labels.pkl'
-gpfs         = '/net/gpfs/home/kohji/Okamura_FGT/cross_validation/'
+file_images  = './headshot_data.npy'
+file_labels  = './headshot_labels.npy'
+dir_data     = './cross_validation/'
 release_size = 359
 iv3_size     = 299
 margin       = 30	# pixel sized of margin
 augmentation = 10	# times of augmentation
 flag_ag      = 0	# save augmented images
 
-
-def read_labels():
-  '''
-  open and read the label pickle data
-  '''
-  import pandas
-  import pickle
-  global head_shot_labels
-  with open(file_labels, 'rb') as pickle_file:
-    head_shot_labels = pickle.load(pickle_file)
-
+import numpy
 
 def read_data():
   '''
-  open and read the image NumPy data
+  open and read the image data
   '''
-  import numpy
-  global head_shot_data
-  head_shot_data = numpy.load(file_images)
+  global headshot_data
+  headshot_data = numpy.load(file_images)
 
+def read_labels():
+  '''
+  open and read the label data
+  '''
+  global headshot_labels
+  headshot_labels = numpy.load(file_labels)
 
 def one_hot_gender_label(is_female):
   '''
   return one-hot gender label in Numpy array
   '''
-  import numpy
-  gender = numpy.empty((1, 2), dtype = 'int8')
-  if is_female:
-    gender[0, 0] = 0
-    gender[0, 1] = 1
-  else:
-    gender[0, 0] = 1
-    gender[0, 1] = 0
-  return gender
-
+  if is_female: return numpy.array((0, 1), dtype = 'int8')
+  else:         return numpy.array((1, 0), dtype = 'int8')	# male
 
 def main():
   '''
   the main function
   '''
   import sys
-  import cv2
-  import numpy
   import keras
-  import pandas
 
-  global head_shot_data
+  global headshot_data, headshot_labels
 
   if keras.backend.image_data_format() != 'channels_last':
-    sys.stderr.write('Error 20: not channels_last' + "\n")
-    sys.exit(20)
+    error = 40
+    sys.stderr.write('Error ' + str(error) + ': not channels_last\n')
+    sys.exit(error)
 
   read_labels()
   read_data()
 
-  n_images = head_shot_data.shape[0]
-  names = head_shot_labels.index
+  n_images = headshot_data.shape[0]
+  names = headshot_labels[:, 0]
   if n_images != len(names):
-    sys.stderr.write('Error 21: ' + str(n_images) + ' ' + str(len(names)) + "\n")
-    sys.exit(21)
+    error = 42
+    sys.stderr.write('Error ' + str(error) + ': ' + str(n_images) + ' ' + str(len(names)) + '\n')
+    sys.exit(error)
 
   volunteers = {}
   squares = numpy.empty((n_images, iv3_size, iv3_size, 3), dtype = 'float16')
   crop3c  = numpy.empty(          (iv3_size, iv3_size, 3), dtype = 'float16')
 
   for i, name in enumerate(names):
-    if head_shot_labels.iat[i, 0] in volunteers:
-      volunteers[head_shot_labels.iat[i, 0]] += "\t" + str(i)
+    if headshot_labels[i, 0] in volunteers:
+      volunteers[headshot_labels[i, 0]] += "\t" + str(i)
     else:
-      volunteers[head_shot_labels.iat[i, 0]] = str(i)
+      volunteers[headshot_labels[i, 0]] = str(i)
 
-    squared = numpy.reshape(head_shot_data[i], (-1, release_size))
+    squared = numpy.reshape(headshot_data[i], (-1, release_size))
     crop = squared[margin:(margin + iv3_size), margin:(margin + iv3_size)]
     for channel in range(3):
       crop3c[:iv3_size, :iv3_size, channel] = crop
@@ -110,36 +96,36 @@ def main():
   for volunteer in sorted(volunteers.keys()):
     lines = volunteers[volunteer].split("\t")
     n_test_images = len(lines)
-    head_shot_data = numpy.empty(((n_images - n_test_images) * augmentation,
-                                   iv3_size, iv3_size, 3), dtype = 'float16')
+    tran_data = numpy.empty(((n_images - n_test_images) * augmentation,
+                    iv3_size, iv3_size, 3), dtype = 'float16')
     test_data = numpy.empty((n_test_images, iv3_size, iv3_size, 3), dtype = 'float16')
-    labels_train = numpy.empty(((n_images - n_test_images) * augmentation, 2), dtype = 'int8')
-    labels_test = numpy.empty((n_test_images, 2), dtype = 'int8')
+    tran_labl = numpy.empty(((n_images - n_test_images) * augmentation, 2), dtype = 'int8')
+    test_labl = numpy.empty((n_test_images, 2), dtype = 'int8')
+
     counter_train, counter_test = 0, 0
     for i in range(n_images):
-      if head_shot_labels.iat[i, 0] == volunteer:
+      if headshot_labels[i, 0] == volunteer:
         test_data[counter_test] = squares[i]
-        labels_test[counter_test] = one_hot_gender_label(head_shot_labels.iat[i, 2])
+        test_labl[counter_test] = one_hot_gender_label(headshot_labels[i, 2])
         counter_test += 1
       else:
         counter_ag = 0
         for ag in augment.flow(squares[i].reshape((1, iv3_size, iv3_size, 3)), batch_size = 1):
-          head_shot_data[counter_train] = ag[0]
-          labels_train[counter_train] = one_hot_gender_label(head_shot_labels.iat[i, 2])
+          tran_data[counter_train] = ag[0]
+          tran_labl[counter_train] = one_hot_gender_label(headshot_labels[i, 2])
           counter_train += 1
           counter_ag += 1
           if flag_ag == 1:
-            file_name = 'augmented/ag_' + head_shot_labels.index[i] + \
+            file_name = 'augmented/ag_{:0=3}'.format(headshot_labels[i]) + \
                 '_{:0=3}'.format(counter_ag) + '.jpeg'
             keras.preprocessing.image.save_img(file_name,
-                keras.preprocessing.image.array_to_img(head_shot_data[counter_train - 1]))
+                keras.preprocessing.image.array_to_img(tran_data[counter_train - 1]))
           if counter_ag >= augmentation:
             break
-    numpy.save(gpfs + volunteer + '_tran_data.npy', head_shot_data)
-    numpy.save(gpfs + volunteer + '_test_data.npy', test_data)
-    numpy.save(gpfs + volunteer + '_tran_labl.npy', labels_train)
-    numpy.save(gpfs + volunteer + '_test_labl.npy', labels_test)
 
+    numpy.save(dir_data + 'lo{:0=3}'.format(volunteer) + '_tran_data.npy', tran_data)
+    numpy.save(dir_data + 'lo{:0=3}'.format(volunteer) + '_test_data.npy', test_data)
+    numpy.save(dir_data + 'lo{:0=3}'.format(volunteer) + '_tran_labl.npy', tran_labl)
+    numpy.save(dir_data + 'lo{:0=3}'.format(volunteer) + '_test_labl.npy', test_labl)
 
-if __name__ == '__main__':
-  main()
+if __name__ == '__main__': main()
